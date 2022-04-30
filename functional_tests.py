e30f380647c7b73f4d0c0e07a1325bb4cae83527
Rachel Brodsky <rb3002@columbia.edu>
2022-04-30 05:00:39 -0700

    Redirect after POST and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -54,7 +54,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a' + 
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a ' + 
                 'fly')
 
         # Edith wonders whether the site will remember her list. Then she sees
