0d061049052dc923dba9dbecb2e2ffa35d75d7c9
Rachel Brodsky <rb3002@columbia.edu>
2022-05-30 20:46:52 -0700

    Implement form class for list items

--- lists/models.py
+++ lists/models.py
@@ -8,6 +8,9 @@ class List(models.Model):
     def get_absolute_url(self):
         return reverse('view_list', args = [self.id])
 
+
 class Item(models.Model):
     text = models.TextField(default = '')
     parent_list = models.ForeignKey(List, default='')
+
+
