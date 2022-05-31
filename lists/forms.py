2cfc831d61d3044913e0ffc962179134a65e5ffb
Rachel Brodsky <rb3002@columbia.edu>
2022-05-30 22:28:10 -0700

    Use new form in home_page, simplify tests. NB breaks stuff

--- lists/forms.py
+++ lists/forms.py
@@ -4,16 +4,15 @@ from lists.models import Item
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 
+
 class ItemForm(forms.models.ModelForm):
 
     class Meta:
-        
+
         model = Item
         fields = ('text',)
-        widgets = {'text': forms.fields.TextInput(attrs = 
-                {'placeholder': 'Enter a to-do item',
-                    'class': 'form-control input-lg',}),}
+        widgets = {'text': forms.fields.TextInput(attrs=
+            {'placeholder': 'Enter a to-do item',
+                'class': 'form-control input-lg', }), }
 
         error_messages = {'text': {'required': EMPTY_ITEM_ERROR}}
-
-
--- lists/models.py
+++ lists/models.py
@@ -3,14 +3,13 @@ from django.core.urlresolvers import reverse
 
 # Create your models here.
 
+
 class List(models.Model):
 
     def get_absolute_url(self):
-        return reverse('view_list', args = [self.id])
+        return reverse('view_list', args=[self.id])
 
 
 class Item(models.Model):
-    text = models.TextField(default = '')
+    text = models.TextField(default='')
     parent_list = models.ForeignKey(List, default='')
-
-
